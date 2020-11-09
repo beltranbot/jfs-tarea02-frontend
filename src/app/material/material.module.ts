6b3870310617ab32b41678c849519acb1aa805af
carlos.beltran <beltranbot@gmail.com>
2020-11-09 16:15:35 -0500

    added app layout

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatDividerModule
+  ]
+})
+export class MaterialModule { }
